a4922eb48b53f893d494410cb453570f02af86e7
AHS CS <ahs-cs@ausdk12.org>
2025-05-30 10:15:14 -0700

    zx

--- Board.java
+++ Board.java
@@ -1,16 +1,13 @@
 
-public class Board  {
-
+public class Board {
 
     private int rows;
     private int cols;
-    
+
     /** The grid of pieces */
     private Player[][] grid;
-    
-    
 
-    public Board(int rows, int cols){
+    public Board(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
         grid = new Player[rows][cols];
@@ -18,7 +15,7 @@ public class Board  {
         reset();
 
     }
-    
+
     public void reset() {
         for (int r = 0; r < rows; r++) {
             for (int c = 0; c < cols; c++) {
@@ -26,53 +23,102 @@ public class Board  {
             }
         }
     }
-    
+
     public int getRows() {
         return rows;
     }
-    
+
     public int getCols() {
         return cols;
     }
-    
-    
+
     /**
-    * Returns the Player whose piece occupies the given location, 
-    * @param row int
-    * @param col int
-    */
-    public Player getCell(int row, int col ) throws IndexOutOfBoundsException{
-        if( (row < 0) || (col < 0) || (row >= rows) || (col >= cols) ) {
+     * Returns the Player whose piece occupies the given location,
+     * 
+     * @param row int
+     * @param col int
+     */
+    public Player getCell(int row, int col) throws IndexOutOfBoundsException {
+        if ((row < 0) || (col < 0) || (row >= rows) || (col >= cols)) {
             throw new IndexOutOfBoundsException();
         } else {
             return grid[row][col];
         }
     }
-    
-    //returns true if there are no more plays left
-    public boolean boardFilled(){
-        //TODO: write this
-        return false; 
-    }
 
-    // Returns true if move is possible given board state.  
-    public boolean possibleMove(Move move) {
-        // TODO: write this.  Right now, it ignores filled columns, claiming any move is possible
+    // returns true if there are no more plays left
+    public boolean boardFilled() {
+        // TODO: write this
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == null) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
-    
+
+    // Returns true if move is possible given board state.
+    public boolean possibleMove(Move move) {
+        // TODO: write this. Right now, it ignores filled columns, claiming any move is
+        // possible
+        for (int r = 0; r < rows; r++) {
+            if (grid[r][move.getColumn()] == null) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
     // Adds a piece to the board for a given Move
     public void addPiece(Move move) {
-        //TODO: this is a test stub, you need to rewrite this.
-    	grid[0][move.getColumn()] = move.getPlayer();
+        // TODO: this is a test stub, you need to rewrite this.
+
+        if (possibleMove(move)) {
+            for (int r = 0; r < grid.length; r++) {
+                if (grid[r][move.getColumn()] == null) {
+                    grid[r][move.getColumn()] = move.getPlayer();
+                    r = grid.length + 1;
+                }
+            }
+        }
     }
 
     // if the board contains a winning position, returns the Player that wins.
-    // Otherwise, returns null.  You could ignore lastMove.
+    // Otherwise, returns null. You could ignore lastMove.
+
     public Player winner(Move lastMove) {
-        // TODO: write this.  Currently, there is never a winnder.
+        for(int r = grid.length - 1; r >= 0; r--){
+            for(int c = 0; c < grid[r].length; c++){
+                Player curPlayer = grid[r][c];
+                if(curPlayer != null) {
+                    if(c + 3 < c && curPlayer == grid[r][c + 1] && curPlayer == grid[r][c + 2] 
+                    && curPlayer == grid[r][c + 3]) {
+                        return curPlayer;
+                    }
+                    if(r - 3 >= 0 && curPlayer == grid[r - 1][c] && curPlayer == grid[r - 2][c] 
+                    && curPlayer == grid[r - 3][c]) {
+                        return curPlayer;
+                    }
+                    if(r - 3 >= 0 && c + 3 < cols && curPlayer == grid[r - 1][c + 1] 
+                    && curPlayer == grid[r - 2][c + 2] && curPlayer == grid[r - 3][c + 3]) {
+                        return curPlayer;
+                    }
+                    if(r + 3 < rows && c + 3 < cols && curPlayer == grid[r + 1][c + 1] 
+                    && curPlayer == grid[r + 2][c + 2] && curPlayer == grid[r + 3][c + 3]) {
+                        return curPlayer;
+                    }
+                }
+            }
+        }
         return null;
-    }
+       
     
     
+}
+
+
+
 } // end Board class
